bb3a71be4a00aeb02bc6e9681316833c51524862
AmirHossein Ebrahimi <amirhosseinebrahimi77@gmail.com>
2022-04-08 13:53:24 +0430

    test.neurons: Default config must be set properly

--- src/safeguards/neurons.py
+++ src/safeguards/neurons.py
@@ -1,26 +1,25 @@
 import unittest
 
+import numpy as np
+
 from PymoNNto import Network, NeuronGroup, Recorder
 from src.core.neurons.neurons import StreamableLIFNeurons
 from src.data.constants import letters
 from src.data.spike_generator import spike_stream_i
 from src.helpers.base import behaviour_generator
 
-V_RESET = -65.0
+LIF_BASE = {
+    "v_rest": -67.0,
+    "v_reset": -65.0,
+    "threshold": -52,
+    "dt": 1.0,
+    "R": 2,
+    "tau": 3,
+}
 
 
-def make_custom_network(corpus):
+def make_custom_network(corpus, use_stream=True, use_long_term_effect=True):
     network = Network()
-
-    lif_base = {
-        "v_rest": -67.0,
-        "v_reset": V_RESET,
-        "threshold": -52,
-        "dt": 1.0,
-        "R": 2,
-        "tau": 3,
-        "stream": [spike_stream_i(char) for char in corpus],
-    }
 
     NeuronGroup(
         net=network,
@@ -28,7 +27,13 @@ def make_custom_network(corpus):
         size=len(letters),
         behaviour=behaviour_generator(
             [
-                StreamableLIFNeurons(**lif_base),
+                StreamableLIFNeurons(
+                    **LIF_BASE,
+                    has_long_term_effect=use_long_term_effect,
+                    stream=[spike_stream_i(char) for char in corpus]
+                    if use_stream
+                    else None,
+                ),
                 Recorder(tag="recorder", variables=["n.v", "n.fired"]),
             ]
         ),
@@ -42,7 +47,6 @@ class MyTestCase(unittest.TestCase):
     def test_network_force_fire_mechanism(self):
         corpus = "abcabab"
         net = make_custom_network(corpus)
-        self.assertEqual(net.iteration, len(corpus))
         ng_fired = net["recorder", 0]["n.fired", 0]
         for fire_pattern, letter in zip(ng_fired, corpus):
             index_letters = letters.index(letter)
@@ -60,7 +64,38 @@ class MyTestCase(unittest.TestCase):
 
         for v, fired in zip(ng_v, ng_fired):
             if fired.any():
-                self.assertEqual(v[fired], V_RESET)
+                self.assertEqual(v[fired], LIF_BASE["v_reset"])
+
+    def test_network_should_use_default_configs(self):
+        corpus = "abc ab a b"
+        net = make_custom_network(corpus, use_long_term_effect=False)
+        ng = net.NeuronGroups[0]
+        for key, value in LIF_BASE.items():
+            if key == "dt":
+                continue
+            self.assertEqual(getattr(ng, key), value)
+
+        net = make_custom_network(corpus)
+        ng = net.NeuronGroups[0]
+        for key, value in LIF_BASE.items():
+            if key == "dt":
+                continue
+            ng_attr = getattr(ng, key)
+            if key == "threshold":
+                self.assertTrue(np.all(ng_attr == ng_attr[0]))
+                self.assertEqual(ng_attr[0], value)
+            else:
+                self.assertEqual(ng_attr, value)
+
+    # TODO: should neuron update itself when no stimulus is applied?
+    # def test_network_must_keeps_voltage_on_no_stimulus(self):
+    #     corpus = "abc ab a b"
+    #     net = make_custom_network(corpus, use_stream=False)
+    #     ng_v = net["recorder", 0]["n.v", 0]
+    #     ng_v0 = ng_v[0]
+    #     for vs in ng_v:
+    #         for v, v0 in zip(vs, ng_v0):
+    #             self.assertEqual(v, v0)
 
 
 if __name__ == "__main__":
