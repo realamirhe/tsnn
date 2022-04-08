48722b145d57b774ac1b128e3ae093301cc66762
AmirHossein Ebrahimi <amirhosseinebrahimi77@gmail.com>
2022-04-08 13:06:49 +0430

    chore: Add plots helpers to visualizer

---
+++ src/core/visualizer/plots.py
@@ -0,0 +1,35 @@
+# ================= Visualization =================
+from matplotlib import pyplot as plt
+
+
+def voltage_visualizer(voltage, title="voltage_trace", labels=("iteration", "voltage")):
+    plt.plot(voltage)
+    plt.title(title)
+    plt.xlabel(labels[0])
+    plt.ylabel(labels[1])
+    plt.show()
+
+
+def spike_visualizer(spikes, title="spike_trace", labels=("iteration", "spike")):
+    if not len(spikes):
+        return
+    plt.imshow(spikes, cmap="gray", aspect="auto")
+    plt.title(title)
+    plt.xlabel(labels[0])
+    plt.ylabel(labels[1])
+    plt.show()
+
+
+def raster_plots(network, ngs=["letters", "words"]):
+    for ng in ngs:
+        spike_visualizer(
+            network[f"{ng}-recorder", 0]["n.fired", 0, "np"].transpose(),
+            title=f"{ng} spike activity",
+        )
+
+
+def voltage_plots(network, ngs=["letters", "words"]):
+    for ng in ngs:
+        voltage_visualizer(
+            network[f"{ng}-recorder", 0]["n.v", 0], title=f"{ng} spike activity",
+        )
