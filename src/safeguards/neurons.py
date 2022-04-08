aed5878dda77c867bf3836d06ac8a476582faf73
AmirHossein Ebrahimi <amirhosseinebrahimi77@gmail.com>
2022-04-08 13:07:10 +0430

    test: Add neurons base safeguards

---
+++ src/safeguards/neurons.py
@@ -0,0 +1,60 @@
+import unittest
+
+from PymoNNto import Network, NeuronGroup, Recorder
+from src.core.neurons.neurons import StreamableLIFNeurons
+from src.data.constants import letters
+from src.data.spike_generator import spike_stream_i
+from src.helpers.base import behaviour_generator
+
+
+def toy_data_stream(corpus):
+    stream = [spike_stream_i(char) for char in corpus]
+    return stream
+
+
+def make_custom_network(corpus):
+    network = Network()
+
+    lif_base = {
+        "v_rest": -65,
+        "v_reset": -65,
+        "threshold": -52,
+        "dt": 1.0,
+        "R": 2,
+        "tau": 3,
+        "stream": toy_data_stream(corpus),
+    }
+
+    NeuronGroup(
+        net=network,
+        tag="letters",
+        size=len(letters),
+        behaviour=behaviour_generator(
+            [
+                StreamableLIFNeurons(**lif_base),
+                Recorder(tag="recorder", variables=["n.v", "n.fired"]),
+            ]
+        ),
+    )
+    network.initialize()
+    network.simulate_iterations(len(corpus))
+    return network
+
+
+class MyTestCase(unittest.TestCase):
+    def test_network_force_fire_mechanism(self):
+        corpus = "abcabab"
+        net = make_custom_network(corpus)
+        self.assertEqual(net.iteration, len(corpus))
+        ng_fired = net["recorder", 0]["n.fired", 0]
+        for fire_pattern, letter in zip(ng_fired, corpus):
+            index_letters = letters.index(letter)
+            for inx in range(len(letters)):
+                if inx == index_letters:
+                    self.assertTrue(fire_pattern[inx])
+                else:
+                    self.assertFalse(fire_pattern[inx])
+
+
+if __name__ == "__main__":
+    unittest.main()
