3c3f962a9194845d508f01370c70b9ca20b6fd12
AmirHossein Ebrahimi <amirhosseinebrahimi77@gmail.com>
2022-05-20 19:25:21 +0430

    refactor: Remove extra history section of wta

--- src/core/stabilizer/winner_take_all.py
+++ src/core/stabilizer/winner_take_all.py
@@ -4,16 +4,8 @@ from PymoNNto import Behaviour
 
 
 class WinnerTakeAll(Behaviour):
-    def set_variables(self, n):
-        self.history = []
-
-    #     assert (
-    #         n.old_v
-    #     ), "ng group must have old v config, please add `capture_old_v=True` to your StreamableLIFNeurons"
-
     def new_iteration(self, n):
         fired = n.fired
-
         if np.sum(fired) > 1:
             temp_fired = n.get_neuron_vec(mode="zeros") > 0
             """ NOTE: old_v can be negative, positive, or zero
@@ -25,18 +17,3 @@ class WinnerTakeAll(Behaviour):
             n.old_v[np.logical_not(fired)] = np.NINF
             temp_fired[np.argmax(n.old_v)] = True
             n.fired = temp_fired
-
-        self.history.append(n.fired.copy())
-
-        if n.iteration == 1:
-            self.history.clear()
-
-        # if n.iteration % 4000 == 0:
-        #     print("history:abc", np.sum([i[0] for i in self.history]), n.iteration)
-        #     print("history:omn", np.sum([i[1] for i in self.history]), n.iteration)
-
-        # testing purposes
-        # assert np.sum(n.fired) <= 1, "More than one neuron fired"
-
-        # out: nan nan nan 1 nan nan nan 2 nan nan nan 0
-        # his: f   f   t  f   f   f   f  f  t  f  f    f
