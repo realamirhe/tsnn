09e2062dfee259299d47e50685a82808a557a91a
AmirHossein Ebrahimi <amirhosseinebrahimi77@gmail.com>
2022-06-05 16:15:30 +0430

    refactor: configuration files

--- src/configs/corpus_config.py
+++ src/configs/corpus_config.py
@@ -1,7 +1,7 @@
 import string
 
-words_spacing_gap = 7
-language = string.ascii_lowercase + " "
+words_spacing_gap = 3
 letters = string.ascii_lowercase
+language = letters + " "  # out_of_vocab_separator
 words = ["abc", "omn"]
 UNK = "UNK"
--- src/configs/plotters.py
+++ src/configs/plotters.py
@@ -1,18 +1,20 @@
 from src.core.visualizer.history_recoreder_1d import HistoryRecorder1D
 from src.core.visualizer.history_recoreder_2d import HistoryRecorder2D
 
-# fmt: off
 dopamine_plotter = HistoryRecorder1D(title="dopamine", window_size=25, enabled=False)
 dw_plotter = HistoryRecorder2D(title="dw", window_size=4, enabled=False)
 w_plotter = HistoryRecorder2D(title="w", window_size=25, enabled=False)
 delay_plotter = HistoryRecorder2D(title="delay", window_size=25, enabled=False)
-selected_dw_plotter = HistoryRecorder1D(title="selected - dw", window_size=25, enabled=False)
-threshold_plotter = HistoryRecorder1D(title="threshold", vertical_history_separator=True)
 activity_plotter = HistoryRecorder1D(title="activity", window_size=25, enabled=False)
-words_stimulus_plotter = HistoryRecorder1D(title="words_stimulus", window_size=25, enabled=False)
-# fmt: on
+selected_dw_plotter = HistoryRecorder1D(
+    title="selected::dw", window_size=25, enabled=False
+)
+threshold_plotter = HistoryRecorder1D(
+    title="threshold", vertical_history_separator=True, enabled=False
+)
+words_stimulus_plotter = HistoryRecorder1D(
+    title="words_stimulus", window_size=25, enabled=False
+)
 selected_delay_plotter = HistoryRecorder1D(
-    title="selected - delay",
-    window_size=25,
-    vertical_history_separator=True,
+    title="selected::delay", window_size=25, enabled=False
 )
