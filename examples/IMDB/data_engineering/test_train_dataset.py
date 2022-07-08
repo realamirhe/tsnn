138d5dc5e9f9ba7fb27243271b0377b724aa38d1
AmirHossein Ebrahimi <amirhosseinebrahimi77@gmail.com>
2022-07-08 15:51:52 +0430

    feat: Add train test split dataset

---
+++ examples/IMDB/data_engineering/test_train_dataset.py
@@ -0,0 +1,37 @@
+from os import getcwd
+from os.path import join
+
+import pandas as pd
+
+RELATIVE_PATH = ""
+
+
+def test_train_dataset(train_size=100, test_size=20):
+    df = pd.read_csv(join(getcwd(), f"{RELATIVE_PATH}corpus/IMDB_preprocessed.csv"))
+    per_class_chunk_size = train_size + test_size
+
+    train_test_positive_class = df[df["sentiment"] == 1].sample(
+        n=per_class_chunk_size, replace=False, random_state=42
+    )
+
+    train_test_negative_class = df[df["sentiment"] == 0].sample(
+        n=per_class_chunk_size, replace=False, random_state=42
+    )
+
+    train_set = [
+        train_test_positive_class.iloc[:train_size],
+        train_test_negative_class.iloc[:train_size],
+    ]
+
+    test_set = [
+        train_test_positive_class.iloc[:test_size],
+        train_test_negative_class.iloc[:test_size],
+    ]
+
+    return pd.concat(train_set), pd.concat(test_set)
+
+
+if __name__ == "__main__":
+    RELATIVE_PATH = "../"
+    train, test = test_train_dataset(train_size=50, test_size=10)
+    print(train.shape, test.shape)
